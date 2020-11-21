a6cc13cd6b89cf54d3db9adbacbd5af8c58a91f2
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-11-21 14:57:22 -0800

    Create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_062446) do
+ActiveRecord::Schema.define(version: 2020_11_21_225237) do
 
   create_table "farms", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_11_13_062446) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
