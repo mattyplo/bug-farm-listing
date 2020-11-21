c56972fd42bb5d63b2932a1e8518ae5f2463c286
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-11-21 15:29:15 -0800

    Add User Passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord   
+  before_save { self.email = email.downcase }
+  has_secure_password
   has_many :farms
   validates :username, presence: true,
                       uniqueness: { case_sensitive: true },
